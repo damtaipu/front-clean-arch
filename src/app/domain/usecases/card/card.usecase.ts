c41cc65c2203aa883d8be04ae45b5696d1dad088
Jose Dam <damiaotaipu@gmail.com>
2024-05-04 08:11:16 -0300

    Project model Front-end Clean Arch

--- src/app/domain/usecases/card/card.usecase.ts
+++ src/app/domain/usecases/card/card.usecase.ts
@@ -1,9 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
-import { CardImplementationsRepository } from '@data/repositories/card/card-implementation.repository';
 import { UseCase } from '@base/usecase/usecase';
 import { Card } from '@domain/models/card/card.models';
+import { CardRepository } from '@domain/repositories/card/card.repository';
 
 
 @Injectable({
@@ -11,9 +11,9 @@ import { Card } from '@domain/models/card/card.models';
 })
 export class CardUseCase implements UseCase<void, Card> {
   
-  private cardRepository = inject(CardImplementationsRepository);
+  private cardRepository = inject(CardRepository);
 
   execute(): Observable<HttpResponse<Card>> {
-    return this.cardRepository.getInitialData()
+    return this.cardRepository.getInitialData();
   }
 }
