49af9e559a2000a40b33177c2eb75fb3ddfb7d2d
Jose Dam <damiaotaipu@gmail.com>
2024-05-05 23:01:05 -0300

    Project model Front-end Clean Arch

--- src/app/shared/project/connection/check-connection.ts
+++ src/app/shared/project/connection/check-connection.ts
@@ -1,35 +1,20 @@
 import { Injectable, inject } from '@angular/core';
-import { MessageService } from 'primeng/api';
-import { Subject, takeUntil } from 'rxjs';
 import { GlobalVerifyConnectionService } from './check-connection.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export abstract class CheckDataConnectionComponent {
-  private messageService = inject(MessageService);
+
   private cnx = inject(GlobalVerifyConnectionService);
 
   checkConnection(): void {
     this.cnx.getConnection().subscribe((x: Event) => {
       if (x.type === 'offline') {
-        this.messageService.add({
-          key: 'cx',
-          sticky: true,
-          closable: false,
-          severity: 'warn',
-          summary: 'Sem conexão!',
-          detail: 'Você ficou sem internet.',
-        });
+        console.log('Disconnected');
       }
       if (x.type === 'online') {
-        this.messageService.clear('cx');
-        this.messageService.add({
-          key: 'cx',
-          severity: 'success',
-          summary: 'Conexão restabelecida!',
-          detail: 'Internet conectada.',
-        });
+        console.log('OnLine');
       }
     });
   }
